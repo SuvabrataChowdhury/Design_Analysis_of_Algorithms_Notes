b58d4cb97e829f6b2a22848fe35a862768fc6076
I578071 <suvabrata.chowdhury@sap.com>
2026-05-31 02:35:12 +0530

    class based modeling

--- src/visualizations/heap/heapify/completeBinaryTree.js
+++ src/visualizations/heap/heapify/completeBinaryTree.js
@@ -1,3 +1,151 @@
+class Node {
+    _highlighted;
+
+    constructor(p5, txt, {x, y, diameter}, highlighted = false, draw = true) {
+        this.p = p5;
+
+        this._txt = txt;
+        this.circParams = {x, y, diameter};
+
+        this.highlighted = highlighted
+
+        if (draw) {
+            this.draw();
+        }
+    }
+
+    moveTo = (x, y) => {
+        this.circParams.x = x;
+        this.circParams.y = y;
+
+        this.draw();
+    }
+
+    get txt() {
+        return this._txt;
+    }
+
+    set txt(txt) {
+        this._txt = txt;
+    }
+
+    get x() {
+        return this.circParams.x;
+    }
+
+    get y() {
+        return this.circParams.y;
+    }
+
+    get highlighted() {
+        return this._highlighted;
+    }
+
+    set highlighted (highlighted) {
+        this._highlighted = highlighted;
+
+        this.draw();
+    }
+
+    draw = () => {
+        this._drawCircle();
+        this._drawText();
+    }
+
+    _drawCircle = () => {
+        this.p.push();
+
+        if (this.highlighted) {
+            this.p.stroke(50, 200, 0);
+            this.p.strokeWeight(10);
+        }
+
+        this.p.circle(this.circParams.x, this.circParams.y, this.circParams.diameter);
+
+        this.p.pop();
+    }
+
+    _drawText = () => {
+        this.p.push();
+
+        if (this.highlighted) {
+            // this.p.strokeWeight(3);
+            this.p.strokeWeight(5);
+            this.p.fill(0, 250, 20);
+        }
+
+        this.p.textAlign(CENTER, CENTER);
+        this.p.textSize(this.circParams.diameter / 2);
+        this.p.text(this.txt, this.circParams.x, this.circParams.y);
+
+        this.p.pop();
+    }
+}
+
+class CompleteBinaryTree {
+    _nodes;
+
+    constructor(p, diameter, rootX, rootY, nodeValues, draw = true) {
+        this.p = p;
+
+        this.diameter = diameter;
+        this.rootX = rootX;
+        this.rootY = rootY;
+
+        this.nodes = nodeValues;
+
+        if(draw) {
+            this.draw();
+        }
+    }
+
+    get nodes() {
+        return this._nodes;
+    }
+
+    set nodes(nodeValues) {
+        this._nodes = this._getNodes(nodeValues);
+
+        // this.draw();
+    }
+
+    _getNodes = (nodeValues) => {
+        let level, numItemsInLevel;
+        // let nodes = [{x: rootX, y: rootY}];
+        let nodes = [new Node(p5 = this.p, nodeValues[0], {x: this.rootX, y: this.rootY, diameter: this.diameter}, draw = false)];
+        
+        for (let i = 1; i < nodeValues.length; i++) {
+            level = Math.floor(Math.log2(i + 1)); // determines the y value
+            numItemsInLevel = Math.pow(2, level);
+
+            let numItemsTillLastLevel = Math.pow(2, level) - 1;
+            let indexInRow = i - numItemsTillLastLevel; // determines the x value
+
+            let y = this.rootY + 80 * level;
+            let x = this.rootX + (150 * (indexInRow - (numItemsInLevel - 1) / 2) * level / (numItemsInLevel - 1));
+
+            nodes.push(new Node(p5 = this.p, nodeValues[i], {x, y, diameter: this.diameter}, draw = false));
+
+            // p.line(x, y, parentX, parentY);
+        }
+
+        return nodes;
+    }
+
+    draw = () => {
+        for (let i = 1; i< this.nodes.length; i++) {
+            let parentX = this.nodes[Math.floor((i + 1) / 2) - 1].x;
+            let parentY = this.nodes[Math.floor((i + 1) / 2) - 1].y;
+           
+            this.p.line(this.nodes[i].x, this.nodes[i].y, parentX, parentY); 
+        }
+
+        for (let i = 0; i<this.nodes.length;i++) {
+            this.nodes[i].draw();
+        }
+    }
+}
+
 /**
  * 
  * @param {import('p5')} p 
@@ -8,15 +156,17 @@
  * @param {number|string} options.txt
  */
 function drawNode(p = window, { x = 0, y = 0, diameter = 10, txt = '' }) {
-    p.push();
+    // p.push();
 
-    p.circle(x, y, diameter);
+    // p.circle(x, y, diameter);
 
-    p.textAlign(CENTER, CENTER);
-    p.textSize(diameter / 2);
-    p.text(txt, x, y);
+    // p.textAlign(CENTER, CENTER);
+    // p.textSize(diameter / 2);
+    // p.text(txt, x, y);
 
-    p.pop();
+    // p.pop();
+
+    return new Node(p5 = p, txt = txt, {x, y, diameter});
 }
 
 /**
@@ -27,7 +177,7 @@ function drawNode(p = window, { x = 0, y = 0, diameter = 10, txt = '' }) {
  * @param {number} options.rootY
  * @param {number[] | string[]} options.nodeValues
  */
-function drawCompleteBinaryTree(p = window, { rootX = 0, rootY = 0, nodeValues = [] }) {
+function drawCompleteBinaryTree(p = window, { rootX = 0, rootY = 0, nodeValues = [] }) { //TODO: Return some object which one can interact
     p.push();
 
     let level, numItemsInLevel;
@@ -50,9 +200,12 @@ function drawCompleteBinaryTree(p = window, { rootX = 0, rootY = 0, nodeValues =
         p.line(x, y, parentX, parentY);
     }
 
+    let nodes = [];
     for (let i = 0; i< nodeValues.length; i++) {
-        drawNode(p, {x: dotPoses[i].x, y: dotPoses[i].y, diameter: 40, txt: nodeValues[i]});
+        nodes.push(drawNode(p, {x: dotPoses[i].x, y: dotPoses[i].y, diameter: 40, txt: nodeValues[i]}));
     }
 
     p.pop();
+
+    return nodes;
 }
--- src/visualizations/heap/heapify/sketch.js
+++ src/visualizations/heap/heapify/sketch.js
@@ -1,13 +1,54 @@
-heapArr = [4, 1, 3, 2, 16, 9, 10, 14, 8, 7, 5, 11, 12, 44, 89]
+const heapArr = [4, 1, 3, 2, 16, 9, 10, 14, 8, 7, 5, 11, 15]
+
+// let circPos = {
+//   x: 0,
+//   y: 0
+// }
+
+let frameCount = 0;
+
+let slider;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  slider = createSlider(0, heapArr.length - 1, 0, 1);
+  slider.position(10,10);
+  slider.size(windowWidth/4, 25);
+  
+  // circPos.x = windowWidth / 2;
+  // circPos.y = windowHeight / 2;
 }
 
 function draw() {
   background(220);
 
-  drawCompleteBinaryTree(window, {rootX: windowWidth/2, rootY: windowHeight/2, nodeValues: heapArr});
+  const windowCenter = {
+    x: windowWidth/2,
+    y: windowHeight/2
+  }
+
+
+  // TODO: Not reacting to slider check
+  let completeBinaryTree = new CompleteBinaryTree(window, 40, windowCenter.x, windowCenter.y, nodeValues = heapArr, draw = false);
+
+  highlightedNode = completeBinaryTree.nodes[slider.value()];
+  highlightedNode.highlighted = true;
+  // completeBinaryTree.nodes = 
+
+  completeBinaryTree.draw();
+
+  // let nodes = completeBinaryTree.nodes;
+  
+  // nodes[slider.value()].highlighted = true;
+
+  // push();
+  // noFill();
+  // stroke(50, 200, 0);
+  // strokeWeight(10);
+  // circle(nodePoses[slider.value()].x, nodePoses[slider.value()].y, 45);
+  // pop();
+
+  // frameCount = (frameCount + 1) % nodePoses.length;
 }
 
 function windowResized() {
